e8c7dbd6e210b27f93da03c9fc90b7c4a6070f6c
unknown <zahirifar.reza@gmail.com>
2025-01-24 14:29:38 +0330

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const blogRoutes = require('./routes/blogRoutes');
+const userRoutes = require('./routes/userRoutes'); // Import user routes
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+// MongoDB connection
+mongoose.connect('mongodb://root:xY6Sx8WNgMHOifqeFXsS2pN8@denali.liara.cloud:31058/my-app?authSource=admin&replicaSet=rs0&directConnection=true', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+// Use blog routes
+app.use('/api/blogs', blogRoutes);
+
+// Use user routes
+app.use('/api/users', userRoutes); // Add user routes
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
